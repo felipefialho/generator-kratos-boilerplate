5c21fe35d8dfe478f632134c49510e234a956eb3
Roberto Achar <robertoachar@gmail.com>
2018-07-18 16:45:10 -0300

    feature: add generator

---
+++ generators/app/validate.js
@@ -0,0 +1,31 @@
+module.exports.validateName = (value) => {
+  if (value.length === 0) {
+    return 'You need to enter your name';
+  }
+
+  return true;
+};
+
+module.exports.validateEmail = (value) => {
+  if (value.length === 0) {
+    return 'You need to enter your email';
+  }
+
+  return true;
+};
+
+module.exports.validateGitHub = (value) => {
+  if (value.length === 0) {
+    return 'You need to enter your GitHub username';
+  }
+
+  return true;
+};
+
+module.exports.validateTwitter = (value) => {
+  if (value.length === 0) {
+    return 'You need to enter your Twitter username';
+  }
+
+  return true;
+};
