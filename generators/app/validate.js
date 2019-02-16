359a2fa527139da0c990f29a2df823b51e210bcb
LFeh <hi@felipefialho.com>
2019-02-16 14:24:00 -0200

    Lint file

--- generators/app/validate.js
+++ generators/app/validate.js
@@ -1,31 +1,19 @@
 module.exports.validateName = (value) => {
-  if (value.length === 0) {
-    return 'You need to enter your name';
-  }
-
+  if (value.length === 0) return 'You need to enter your name';
   return true;
 };
 
 module.exports.validateEmail = (value) => {
-  if (value.length === 0) {
-    return 'You need to enter your email';
-  }
-
+  if (value.length === 0) return 'You need to enter your email';
   return true;
 };
 
 module.exports.validateGitHub = (value) => {
-  if (value.length === 0) {
-    return 'You need to enter your GitHub username';
-  }
-
+  if (value.length === 0) return 'You need to enter your GitHub username';
   return true;
 };
 
 module.exports.validateTwitter = (value) => {
-  if (value.length === 0) {
-    return 'You need to enter your Twitter username';
-  }
-
+  if (value.length === 0) return 'You need to enter your Twitter username';
   return true;
 };
