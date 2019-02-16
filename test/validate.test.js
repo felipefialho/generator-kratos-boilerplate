cbe8634b2d8c3588b429e7e44236c38edd920a4b
LFeh <hi@felipefialho.com>
2019-02-16 14:25:49 -0200

    Adjust test

--- test/validate.test.js
+++ test/validate.test.js
@@ -5,65 +5,49 @@ const validate = require('../generators/app/validate');
 describe('Validate Tests', () => {
   it('should return true with name', (done) => {
     const message = validate.validateName('Felipe Fialho');
-
     assert.equal(message, true);
-
     done();
   });
 
   it('should return an error without name', (done) => {
     const message = validate.validateName('');
-
     assert.equal(message, 'You need to enter your name');
-
     done();
   });
 
   it('should return true with email', (done) => {
     const message = validate.validateEmail('hi@felipefialho.com');
-
     assert.equal(message, true);
-
     done();
   });
 
   it('should return an error without email', (done) => {
     const message = validate.validateEmail('');
-
     assert.equal(message, 'You need to enter your email');
-
     done();
   });
 
   it('should return true with GitHub username', (done) => {
     const message = validate.validateGitHub('LFeh');
-
     assert.equal(message, true);
-
     done();
   });
 
   it('should return an error without GitHub username', (done) => {
     const message = validate.validateGitHub('');
-
     assert.equal(message, 'You need to enter your GitHub username');
-
     done();
   });
 
   it('should return true with Twitter username', (done) => {
     const message = validate.validateTwitter('LFeh');
-
     assert.equal(message, true);
-
     done();
   });
 
   it('should return an error without Twitter username', (done) => {
     const message = validate.validateTwitter('');
-
     assert.equal(message, 'You need to enter your Twitter username');
-
     done();
   });
 });
